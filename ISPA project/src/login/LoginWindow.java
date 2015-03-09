577a943abce0035ccefff38dd07ed95d01885d27
Alexander <abelmore91@gmail.com>
2015-03-09 17:44:16 -0400

    Added login function. Not currently working due to to errors elsewhere in project.

--- ISPA project/src/login/LoginWindow.java
+++ ISPA project/src/login/LoginWindow.java
@@ -3,6 +3,12 @@ package login;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -49,5 +55,56 @@ public class LoginWindow {
 	    
 		frame.getContentPane().add(buttonGo);
 		frame.setVisible(true);
+	}
+	
+	public static int loginAttempt(String name, String pass) throws SQLException{
+		Statement stmt = null;
+		Connection con;
+		int loginType = 0;
+
+			
+		try {
+			String query = "SELECT * FROM logins;";
+			String username = "cis499";
+			final String password = "password";
+		    final String driverClass = "com.mysql.jdbc.Driver";
+		    final String url = "jdbc:mysql://scraggley.cis.umassd.edu/" + username;
+			
+			con = DriverManager.getConnection(url, username, password);
+	        // Create a Statement
+	        Statement stmnt = con.createStatement();
+
+	        // Execute a query
+	        ResultSet rs = stmnt.executeQuery(query);
+				
+	        ResultSetMetaData rsmd = rs.getMetaData();
+	        int columnsNumber = rsmd.getColumnCount();
+	        
+	        try{
+				while (rs.next()){
+					//System.out.println(rs.getString("username")+", "+rs.getString("password")+", "+rs.getInt("type"));
+					int userMatch = name.compareTo(rs.getString("username"));
+					//System.out.println("Comp is " +userMatch);
+					int passMatch = pass.compareTo(rs.getString("password"));
+					//System.out.println("Comp is " +passMatch);
+					
+					if(userMatch==0 && passMatch==0){
+						loginType = rs.getInt("type");
+						return loginType;
+					}
+				}
+			} catch (SQLException e){
+					
+			} finally{
+				if(stmt!= null) {
+					stmt.close();
+				}
+			}	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		return 0;
 	}
 }
