4db615048bb2fdb517e42ec2f67082729fb235c7
Alexander <abelmore91@gmail.com>
2015-03-24 16:10:39 -0400

    Restoring

--- ISPA project/src/login/LoginWindow.java
+++ ISPA project/src/login/LoginWindow.java
@@ -110,35 +110,6 @@ public class LoginWindow {
 		});
 	}
 	
-	/*
-	private void initialize(){
-		// This is the login window
-		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JLabel labelLogin = new JLabel("IPSA Login");
-		labelLogin.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
-		labelLogin.setBounds(357, 111, 132, 53);
-		
-		frame.getContentPane().add(labelLogin);
-	
-		JButton buttonGo = new JButton("Go");
-		buttonGo.setBounds(357, 203, 117, 29);
-	    buttonGo.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
-	    		frame.dispose();
-	    		MainWindow mw = new MainWindow();
-	    		mw.setVisible(true);
-	    	}
-	    });		
-	    
-		frame.getContentPane().add(buttonGo);
-		frame.setVisible(true);
-	}
-	*/
-
 	public static int loginAttempt(String name, String pass) throws SQLException{
 		Statement stmt = null;
 		Connection con;
